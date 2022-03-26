2127ddd9f10ac5beda4a5cf30a13a5de30c95ee7
owen huang <ohuang2@gmail.com>
2022-03-26 17:26:32 -0400

    Map dislike function to the react pages

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { checkIfUserLikedTuit } from "../../services/likes-service";
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+
+const TuitStats = ({tuit, likeTuit = () => {},dislikeTuit = () => {}}) => {
+
     return (
       <div className="row mt-2">
         <div className="col">
@@ -13,13 +16,33 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
         </div>
         <div className="col">
           <span onClick={() => likeTuit(tuit)}>
+            
               {
+                  
                   tuit.stats.likes > 0 &&
                   <i className="fa-solid fa-thumbs-up" style={{color: 'red'}}></i>
+                
               }
               {
                   tuit.stats.likes <= 0 &&
                   <i className="fa-solid fa-thumbs-up"></i>
+              }
+              
+            {tuit.stats && tuit.stats.likes}
+          </span>
+        </div>
+        <div className="col">
+          <span onClick={() => dislikeTuit(tuit)}>
+            
+              {
+                  
+                  tuit.stats.likes > 0 &&
+                  <i className="fa-solid fa-thumbs-down" style={{color: 'red'}}></i>
+                
+              }
+              {
+                  tuit.stats.likes <= 0 &&
+                  <i className="fa-solid fa-thumbs-down"></i>
               }
               
             {tuit.stats && tuit.stats.likes}
